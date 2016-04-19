b3e97bc1fd70e162232b881f8d6d49959c5ba8af
Stuart Yamartino <stuart@yamartino.com>
2016-04-18 20:55:49 -0400

    Pass through option for short form expiration date
    Also when I compiled the jQuery version it also added a lot of other
    stuff so it seems like jQuery was not compiled for the last release.
    Just a heads up.

--- dist/jquery.payform.js
+++ dist/jquery.payform.js
@@ -9,8 +9,8 @@ payform = require(2);
     formatCardNumber: function() {
       return payform.cardNumberInput(this.get(0));
     },
-    formatCardExpiry: function() {
-      return payform.expiryInput(this.get(0));
+    formatCardExpiry: function(option) {
+      return payform.expiryInput(this.get(0), option);
     },
     formatCardCVC: function() {
       return payform.cvcInput(this.get(0));
@@ -19,9 +19,9 @@ payform = require(2);
       return payform.numericInput(this.get(0));
     }
   };
-  return $.fn.payform = function(method) {
+  return $.fn.payform = function(method, option) {
     if ($.payform.fn[method] != null) {
-      $.payform.fn[method].call(this);
+      $.payform.fn[method].call(this, option);
     }
     return this;
   };
@@ -29,7 +29,7 @@ payform = require(2);
 
 
 
-},{}],2:[function(require,module,exports){
+},{"2":2}],2:[function(require,module,exports){
 
 /*
   Payform Javascript Library
@@ -39,7 +39,7 @@ payform = require(2);
   License: MIT
   Version: 1.1.0
  */
-var __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
+var indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
 (function(name, definition) {
   if (typeof module !== "undefined" && module !== null) {
@@ -50,7 +50,7 @@ var __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; 
     return this[name] = definition();
   }
 })('payform', function() {
-  var cardFromNumber, cardFromType, defaultFormat, formatBackCardNumber, formatBackExpiry, formatCardExpiry, formatCardNumber, formatForwardExpiry, formatForwardSlashAndSpace, hasTextSelected, luhnCheck, payform, reFormatCVC, reFormatCardNumber, reFormatExpiry, restrictCVC, restrictCardNumber, restrictExpiry, restrictNumeric, _eventNormalize, _getCaretPos, _on;
+  var _eventNormalize, _getCaretPos, _on, cardFromNumber, cardFromType, defaultFormat, formatBackCardNumber, formatBackExpiry, formatCardExpiry, formatCardNumber, formatForwardExpiry, formatForwardSlashAndSpace, hasTextSelected, luhnCheck, payform, reFormatCVC, reFormatCardNumber, reFormatExpiry, replaceFullWidthChars, restrictCVC, restrictCardNumber, restrictExpiry, restrictNumeric;
   _getCaretPos = function(ele) {
     var r, rc, re;
     if (ele.selectionStart != null) {
@@ -128,7 +128,7 @@ var __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; 
       luhn: true
     }, {
       type: 'mastercard',
-      pattern: /^5[0-5]/,
+      pattern: /^(5[1-5]|2[2-7])/,
       format: defaultFormat,
       length: [16],
       cvcLength: [3],
@@ -143,7 +143,7 @@ var __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; 
     }, {
       type: 'dinersclub',
       pattern: /^3[0689]/,
-      format: defaultFormat,
+      format: /(\d{1,4})(\d{1,6})?(\d{1,4})?/,
       length: [14],
       cvcLength: [3],
       luhn: true
@@ -171,33 +171,33 @@ var __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; 
     }
   ];
   cardFromNumber = function(num) {
-    var card, _i, _len, _ref;
+    var card, i, len, ref;
     num = (num + '').replace(/\D/g, '');
-    _ref = payform.cards;
-    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-      card = _ref[_i];
+    ref = payform.cards;
+    for (i = 0, len = ref.length; i < len; i++) {
+      card = ref[i];
       if (card.pattern.test(num)) {
         return card;
       }
     }
   };
   cardFromType = function(type) {
-    var card, _i, _len, _ref;
-    _ref = payform.cards;
-    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-      card = _ref[_i];
+    var card, i, len, ref;
+    ref = payform.cards;
+    for (i = 0, len = ref.length; i < len; i++) {
+      card = ref[i];
       if (card.type === type) {
         return card;
       }
     }
   };
   luhnCheck = function(num) {
-    var digit, digits, odd, sum, _i, _len;
+    var digit, digits, i, len, odd, sum;
     odd = true;
     sum = 0;
     digits = (num + '').split('').reverse();
-    for (_i = 0, _len = digits.length; _i < _len; _i++) {
-      digit = digits[_i];
+    for (i = 0, len = digits.length; i < len; i++) {
+      digit = digits[i];
       digit = parseInt(digit, 10);
       if ((odd = !odd)) {
         digit *= 2;
@@ -210,13 +210,32 @@ var __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; 
     return sum % 10 === 0;
   };
   hasTextSelected = function(target) {
-    var _ref;
-    if ((typeof document !== "undefined" && document !== null ? (_ref = document.selection) != null ? _ref.createRange : void 0 : void 0) != null) {
+    var ref;
+    if ((typeof document !== "undefined" && document !== null ? (ref = document.selection) != null ? ref.createRange : void 0 : void 0) != null) {
       if (document.selection.createRange().text) {
         return true;
       }
     }
     return (target.selectionStart != null) && target.selectionStart !== target.selectionEnd;
+  };
+  replaceFullWidthChars = function(str) {
+    var char, chars, fullWidth, halfWidth, i, idx, len, value;
+    if (str == null) {
+      str = '';
+    }
+    fullWidth = '\uff10\uff11\uff12\uff13\uff14\uff15\uff16\uff17\uff18\uff19';
+    halfWidth = '0123456789';
+    value = '';
+    chars = str.split('');
+    for (i = 0, len = chars.length; i < len; i++) {
+      char = chars[i];
+      idx = fullWidth.indexOf(char);
+      if (idx > -1) {
+        char = halfWidth[idx];
+      }
+      value += char;
+    }
+    return value;
   };
   reFormatCardNumber = function(e) {
     var cursor;
@@ -354,7 +373,7 @@ var __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; 
   reFormatCVC = function(e) {
     var cursor;
     cursor = _getCaretPos(e.target);
-    e.target.value = e.target.value.replace(/\D/g, '').slice(0, 4);
+    e.target.value = replaceFullWidthChars(e.target.value).replace(/\D/g, '').slice(0, 4);
     if ((cursor != null) && e.type !== 'change') {
       return e.target.setSelectionRange(cursor, cursor);
     }
@@ -392,7 +411,7 @@ var __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; 
       return e.preventDefault();
     }
   };
-  restrictExpiry = function(e) {
+  restrictExpiry = function(short, e) {
     var digit, value;
     digit = String.fromCharCode(e.which);
     if (!/^\d+$/.test(digit)) {
@@ -403,7 +422,7 @@ var __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; 
     }
     value = e.target.value + digit;
     value = value.replace(/\D/g, '');
-    if (value.length > 6) {
+    if (value.length > (short ? 4 : 6)) {
       return e.preventDefault();
     }
   };
@@ -428,9 +447,12 @@ var __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; 
     _on(input, 'change', reFormatCVC);
     return _on(input, 'input', reFormatCVC);
   };
-  payform.expiryInput = function(input) {
+  payform.expiryInput = function(input, short) {
+    if (short == null) {
+      short = false;
+    }
     _on(input, 'keypress', restrictNumeric);
-    _on(input, 'keypress', restrictExpiry);
+    _on(input, 'keypress', restrictExpiry.bind(this, short));
     _on(input, 'keypress', formatCardExpiry);
     _on(input, 'keypress', formatForwardSlashAndSpace);
     _on(input, 'keypress', formatForwardExpiry);
@@ -454,9 +476,9 @@ var __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; 
     return _on(input, 'input', restrictNumeric);
   };
   payform.parseCardExpiry = function(value) {
-    var month, prefix, year, _ref;
+    var month, prefix, ref, year;
     value = value.replace(/\s/g, '');
-    _ref = value.split('/', 2), month = _ref[0], year = _ref[1];
+    ref = value.split('/', 2), month = ref[0], year = ref[1];
     if ((year != null ? year.length : void 0) === 2 && /^\d+$/.test(year)) {
       prefix = (new Date).getFullYear();
       prefix = prefix.toString().slice(0, 2);
@@ -470,7 +492,7 @@ var __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; 
     };
   };
   payform.validateCardNumber = function(num) {
-    var card, _ref;
+    var card, ref;
     num = (num + '').replace(/\s+|-/g, '');
     if (!/^\d+$/.test(num)) {
       return false;
@@ -479,12 +501,12 @@ var __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; 
     if (!card) {
       return false;
     }
-    return (_ref = num.length, __indexOf.call(card.length, _ref) >= 0) && (card.luhn === false || luhnCheck(num));
+    return (ref = num.length, indexOf.call(card.length, ref) >= 0) && (card.luhn === false || luhnCheck(num));
   };
   payform.validateCardExpiry = function(month, year) {
-    var currentTime, expiry, _ref;
+    var currentTime, expiry, ref;
     if (typeof month === 'object' && 'month' in month) {
-      _ref = month, month = _ref.month, year = _ref.year;
+      ref = month, month = ref.month, year = ref.year;
     }
     if (!(month && year)) {
       return false;
@@ -517,27 +539,28 @@ var __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; 
     return expiry > currentTime;
   };
   payform.validateCardCVC = function(cvc, type) {
-    var card, _ref;
+    var card, ref;
     cvc = String(cvc).trim();
     if (!/^\d+$/.test(cvc)) {
       return false;
     }
     card = cardFromType(type);
     if (card != null) {
-      return _ref = cvc.length, __indexOf.call(card.cvcLength, _ref) >= 0;
+      return ref = cvc.length, indexOf.call(card.cvcLength, ref) >= 0;
     } else {
       return cvc.length >= 3 && cvc.length <= 4;
     }
   };
   payform.parseCardType = function(num) {
-    var _ref;
+    var ref;
     if (!num) {
       return null;
     }
-    return ((_ref = cardFromNumber(num)) != null ? _ref.type : void 0) || null;
+    return ((ref = cardFromNumber(num)) != null ? ref.type : void 0) || null;
   };
   payform.formatCardNumber = function(num) {
-    var card, groups, upperLength, _ref;
+    var card, groups, ref, upperLength;
+    num = replaceFullWidthChars(num);
     num = num.replace(/\D/g, '');
     card = cardFromNumber(num);
     if (!card) {
@@ -546,7 +569,7 @@ var __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; 
     upperLength = card.length[card.length.length - 1];
     num = num.slice(0, upperLength);
     if (card.format.global) {
-      return (_ref = num.match(card.format)) != null ? _ref.join(' ') : void 0;
+      return (ref = num.match(card.format)) != null ? ref.join(' ') : void 0;
     } else {
       groups = card.format.exec(num);
       if (groups == null) {
@@ -559,6 +582,7 @@ var __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; 
   };
   payform.formatCardExpiry = function(expiry) {
     var mon, parts, sep, year;
+    expiry = replaceFullWidthChars(expiry);
     parts = expiry.match(/^\D*(\d{1,2})(\D+)?(\d{1,4})?/);
     if (!parts) {
       return '';
--- src/jquery.payform.coffee
+++ src/jquery.payform.coffee
@@ -6,13 +6,13 @@ do ($ = window.jQuery || window.Zepto) ->
   $.payform.fn =
     formatCardNumber: ->
       payform.cardNumberInput @get(0)
-    formatCardExpiry: ->
-      payform.expiryInput @get(0)
+    formatCardExpiry: (option)->
+      payform.expiryInput @get(0), option
     formatCardCVC: ->
       payform.cvcInput @get(0)
     formatNumeric: ->
       payform.numericInput @get(0)
 
-  $.fn.payform = (method) ->
-    $.payform.fn[method].call(this) if $.payform.fn[method]?
+  $.fn.payform = (method, option) ->
+    $.payform.fn[method].call(this, option) if $.payform.fn[method]?
     return this
